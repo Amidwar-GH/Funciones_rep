a15d303af88780d36e54ac2e67474bfc0cea5607
Amidwar <amidwar05@gmail.com>
2025-08-30 18:30:02 -0500

    Ejercicio de potencia con funciones terminado

--- EJERCICIO3.cpp
+++ EJERCICIO3.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 void datos();
+void funpot(int numero, int exp);
 
-
-int numero,exp;
+int numero ,exp;
 
 int main(){
 	
 	datos();
+	funpot(numero,exp);
 	
 	return 0;
 }
@@ -16,4 +17,14 @@ int main(){
 void datos(){
 	cout<<"Digite el numero: "; cin>>numero;
 	cout<<"Digite el exponente: "; cin>>exp;
+}
+
+void funpot(int numero, int exp){
+	
+	long resultado=1;
+	for(int i=0; i<exp; i++){
+		resultado *= numero;
+	}
+	
+	cout<<"El resultado es: "<<resultado;
 }
