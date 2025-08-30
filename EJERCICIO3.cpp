3653bf9b41a4b0bb21c3d5a37bd46308a472d1e1
Amidwar <amidwar05@gmail.com>
2025-08-30 18:21:22 -0500

    Creando la funcion de peticion de datos y llamandola desde el main

---
+++ EJERCICIO3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void datos();
+
+
+int numero,exp;
+
+int main(){
+	
+	datos();
+	
+	return 0;
+}
+
+void datos(){
+	cout<<"Digite el numero: "; cin>>numero;
+	cout<<"Digite el exponente: "; cin>>exp;
+}
