a031936c2f00a1b8c4deb0c93f124f055323d619
Amidwar <amidwar05@gmail.com>
2025-09-03 22:53:17 -0500

    Agregando el ejercicio 10 de calculo de año

---
+++ EJERCICIOS/EJERCICIO10.cpp
@@ -0,0 +1,20 @@
+//Ejercicio 10
+
+#include<iostream>
+using namespace std;
+
+void calc_años(int, int&, in&, int&);
+
+int main(){
+	int num, anio=2000, mes=1, dia=1;
+	cout<<"Digite un numero de dias: "; cin>>num;
+	calc_años(num,anio,mes,dia);
+	
+	return 0;
+}
+
+void calc_años(int, int&anio, in&mes, int&dia){
+	
+	
+	
+}
