6302a9e442f539a0c326c853d30f6a7cccfe2de1
Amidwar <amidwar05@gmail.com>
2025-09-03 23:05:44 -0500

    Mejorando el algoritmo de calculo de años

--- EJERCICIOS/EJERCICIO10.cpp
+++ EJERCICIOS/EJERCICIO10.cpp
@@ -3,19 +3,25 @@
 #include<iostream>
 using namespace std;
 
-void calc_años(int, int&, in&, int&);
+void calc_anios(int, int&, int&, int&);
 
 int main(){
 	int num, anio=2000, mes=1, dia=1;
 	cout<<"Digite un numero de dias: "; cin>>num;
-	calc_años(num,anio,mes,dia);
+	calc_anios(num,anio,mes,dia);
+	
+	cout<<"La fecha actual es: 01/01/200\n";
+	cout<<"La fecha nueva es: "<<dia<<" / "<<mes<<" / "<<anio;
 	
 	return 0;
 }
 
-void calc_años(int, int&anio, in&mes, int&dia){
-	anio = num / 365;
+void calc_anios(int num, int&anio, int&mes, int&dia){
+	anio = anio + (num / 365);
 	num %= 365;
-	mes = num / 30
-	dia = num % 30;
+	
+	mes = mes + (num / 30);
+	num = num % 30;
+	
+	dia = dia + num;
 }
