123860678fc36de3f69736a7360bf0fda67082d8
Amidwar <amidwar05@gmail.com>
2025-09-04 21:35:39 -0500

    Agregando la muestra y docuementacion de las funciones

--- EJEMPLOS/EJEMPLO DE FUNCION CON ARREGLO.cpp
+++ EJEMPLOS/EJEMPLO DE FUNCION CON ARREGLO.cpp
@@ -9,21 +9,34 @@ Parametros de la funcion :
 #include<iostream>
 using namespace std;
 
+//prototipos de funcion
 void cuadrado(int vec[], int);
+void Mostrar(int arr[], int );
 
 int main(){
 	const int TAM=5;
 	int vec[TAM] = {1,2,3,4,5};
 	
+	//Llamado de la funcion
+	cuadrado(vec,TAM); // <- No es necesario poner vec[], omitir los corchetes
 	
-	
+	//Mostrando los elementos de la funcion
+	Mostrar(vec, TAM);
 	
 	return 0;
 }
 
+//Funcion para elevar los elementos del vector al cuadrado
 void cuadrado(int vec[], int tam){
 	for(int i=0; i<tam; i++){
 		vec[i] = vec[i] * vec[i];
 	}
 	
+}
+
+//Funcion para mostrar los elementos del arreglo
+void Mostrar(int arr[], int tam){
+	for(int i=0; i<tam; i++){
+		cout<<arr[i]<<" ";
+	}
 }
