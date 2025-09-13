a968de16edf5e19047e18e8035816cf5e6f4b114
Amidwar <amidwar05@gmail.com>
2025-09-12 22:16:46 -0500

    Commit

--- EJERCICIOS/EJERCICIO10.cpp
+++ EJERCICIOS/EJERCICIO10.cpp
@@ -8,7 +8,7 @@ void calc_anios(int, int&, int&, int&);
 
 int main(){
 	int num, anio=2000, mes=1, dia=1;
-	cout<<"Digite un numero de dias: "; cin>>num;
+	cout<<"Digite un numero de dias para la conversion: "; cin>>num;
 	calc_anios(num,anio,mes,dia);
 	
 	cout<<"La fecha actual es: 01/01/200\n";
