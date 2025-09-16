a14a241768457918b22070f07f5306f23aefefb4
Amidwar <amidwar05@gmail.com>
2025-09-16 13:10:54 -0500

    Agregando archivos

---
+++ EJERCICIOS/EJERCICIO15.cpp
@@ -0,0 +1,22 @@
+/*Ejercicio 15: Desarrolle una funcion que determine si una matriz es simetrica o no
+
+Una matriz es simetrica : A=A^t;
+
+-La matriz debe ser cuadrada 
+- Aij = Aji
+
+/2  5  9/      	/2  5  9/
+/5  8 -1/   -> 	/5  8 -1/ 
+/9 -1 10/		/9 -1 10/
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	
+	
+	return 0;
+}
