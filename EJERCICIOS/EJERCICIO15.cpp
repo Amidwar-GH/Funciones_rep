8b71ce36a1146b460cfcefeb5f5836db43495ddd
Amidwar <amidwar05@gmail.com>
2025-09-27 21:42:26 -0500

    Desarrollando la funcion de pedir datos

--- EJERCICIOS/EJERCICIO15.cpp
+++ EJERCICIOS/EJERCICIO15.cpp
@@ -14,9 +14,27 @@ Una matriz es simetrica : A=A^t;
 #include<iostream>
 using namespace std;
 
+
+void pedirDatos();
+
+
+int m[100][100], nfilas, ncol; 
+
 int main(){
-	
+	pedirDatos();
 	
 	
 	return 0;
+}
+
+void pedirDatos(){
+	cout<<"Digite el numero de filas: "; cin>>nfilas;
+	cout<<"Digite el numero de columnas: "; cin>>ncol;
+	
+	for(int i=0; i<nfilas; i++){
+		for(int j=0; j<ncol; j++){
+			cout<<"Digite un numero ["<<i<<"]["<<j<<"]: ";
+			cin>>m[i][j];
+		}
+	}
 }
